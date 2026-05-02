6c47d68714111606e5bbb7c1d3548c7d206a9237
CadeJordan <cade.m.jordan@gmail.com>
2026-05-02 11:01:20 -0700

    setup

---
+++ web-crawler/crawler.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Part A web crawler launcher (Windows)
+REM Intended usage (after implementation):
+REM   crawler.bat ^<seed-File^> ^<num-pages^> ^<hops-away^> ^<output-dir^> [optional domain filter args]
+REM Example:
+REM   crawler.bat data\seed.txt 10000 6 output\html
---
+++ web-crawler/crawler.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Part A web crawler launcher (Unix/Linux/macOS)
+# Intended usage (after implementation):
+#   ./crawler.sh <seed-File> <num-pages> <hops-away> <output-dir> [optional domain filter args]
+# Example:
+#   ./crawler.sh data/seed.txt 10000 6 output/html
