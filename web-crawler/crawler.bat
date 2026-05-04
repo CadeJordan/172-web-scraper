46e31e26529d19a3013ce22e23d082f36e5ff1f9
Jooahn Park <jpark8577@gmail.com>
2026-05-04 15:22:16 -0700

    added missing files

--- web-crawler/crawler.bat
+++ web-crawler/crawler.bat
@@ -4,3 +4,9 @@ REM Intended usage (after implementation):
 REM   crawler.bat ^<seed-File^> ^<num-pages^> ^<hops-away^> ^<output-dir^> [optional domain filter args]
 REM Example:
 REM   crawler.bat data\seed.txt 10000 6 output\html
+
+scrapy crawl tech_news ^
+  -a seed_file=%1 ^
+  -a max_pages=%2 ^
+  -a max_depth=%3 ^
+  -s OUTPUT_DIR=%4
--- web-crawler/crawler.sh
+++ web-crawler/crawler.sh
@@ -4,3 +4,16 @@
 #   ./crawler.sh <seed-File> <num-pages> <hops-away> <output-dir> [optional domain filter args]
 # Example:
 #   ./crawler.sh data/seed.txt 10000 6 output/html
+
+#!/bin/bash
+# Usage: ./crawler.sh <seed_file> <max_pages> <max_depth> <output_dir>
+SEED_FILE=${1:-seeds.txt}
+MAX_PAGES=${2:-10000}
+MAX_DEPTH=${3:-6}
+OUTPUT_DIR=${4:-crawled_pages}
+
+scrapy crawl tech_news \
+  -a seed_file=$SEED_FILE \
+  -a max_pages=$MAX_PAGES \
+  -a max_depth=$MAX_DEPTH \
+  -s OUTPUT_DIR=$OUTPUT_DIR
