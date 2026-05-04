d3d5553a33d53bf3b4424599589af555a907a76b
Cade Jordan <117262662+CadeJordan@users.noreply.github.com>
2026-05-04 03:28:36 -0700

    Revert "Scrapy crawler"

--- web-crawler/scrapy_project/technews_crawler/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class CrawledPage(scrapy.Item):
-    url = scrapy.Field()
-    depth = scrapy.Field()
-    status = scrapy.Field()
-    content_type = scrapy.Field()
-    body = scrapy.Field()
